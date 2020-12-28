1c77807ad61a51f79f2b7765c220adb6058a50f2
ducphucnguyen <71771692+ducphucnguyen@users.noreply.github.com>
2020-12-28 10:34:27 +0000

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,32 @@
 using Documenter, FreeRay
 
-makedocs(sitename="FreeRay")
+makedocs(
+    modules = [FreeRay],
+    doctest = true,
+    sitename = "FreeRay",
+    authors = "Duc-Phuc Nguyen",
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true",
+        assets     = ["assets/freeray.css"],
+    ),
+    pages = [
+        "Home"              => "index.md",
+        "Getting Started"   => [
+            "Introduction to Rendering" => "getting_started/teapot_rendering.md",
+            "Inverse Lighting"          => "getting_started/inverse_lighting.md",
+            "Optimizing using Optim.jl" => "getting_started/optim_compatibility.md",
+        ],
+        "API Documentation" => [
+            "General Utilities"         => "api/utilities.md",
+            "Differentiation"           => "api/differentiation.md",
+            "Scene Configuration"       => "api/scene.md",
+            "Renderers"                 => "api/renderers.md",
+            "Optimization"              => "api/optimization.md",
+            "Acceleration Structures"   => "api/accelerators.md",
+        ],
+    ],
+)
+
+deploydocs(
+    repo = "github.com/ducphucnguyen/FreeRay.jl.git"
+)
