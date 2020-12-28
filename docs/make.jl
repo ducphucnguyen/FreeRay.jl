974cc9c131ac7925defd9073b7c923b5a90127fa
Duc Phuc Nguyen <ducphuc.nguyen@flinders.edu.au>
2020-12-28 21:14:47 +1030

    update docs

--- docs/make.jl
+++ docs/make.jl
@@ -12,17 +12,17 @@ makedocs(
     pages = [
         "Home"              => "index.md",
         "Getting Started"   => [
-            "Introduction to Rendering" => "getting_started/teapot_rendering.md",
+            #Introduction to Rendering" => "getting_started/teapot_rendering.md",
             "Inverse Lighting"          => "getting_started/inverse_lighting.md",
-            "Optimizing using Optim.jl" => "getting_started/optim_compatibility.md",
+            #"Optimizing using Optim.jl" => "getting_started/optim_compatibility.md",
         ],
         "API Documentation" => [
-            "General Utilities"         => "api/utilities.md",
-            "Differentiation"           => "api/differentiation.md",
-            "Scene Configuration"       => "api/scene.md",
+            #"General Utilities"         => "api/utilities.md",
+            #"Differentiation"           => "api/differentiation.md",
+            #"Scene Configuration"       => "api/scene.md",
             "Renderers"                 => "api/renderers.md",
-            "Optimization"              => "api/optimization.md",
-            "Acceleration Structures"   => "api/accelerators.md",
+            #"Optimization"              => "api/optimization.md",
+            #"Acceleration Structures"   => "api/accelerators.md",
         ],
     ],
 )
